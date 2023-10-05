a833c722062ce6ae21236f6c678d62951db18237
Seongmin Jo <seojo@c3r2s7.42seoul.kr>
2023-10-05 21:14:21 +0900

    s

--- libs/listener/Asocket.hpp
+++ libs/listener/Asocket.hpp
@@ -1,38 +1,56 @@
 #pragma once
-
+ 
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
 class ASocket
 {
 	public:
-		virtual ~ASocket()
+		virtual ~ASocket();
+		ASocket();
 		ASocket(const int port);
-		ASocket(const int port, const char * address);
 		ASocket(const ASocket & other);
+		ASocket(const int port, const char * address);
+
 		ASocket& operator = (const ASocket & other);
 
 		virtual int handle();
 
 	private:
-		ASocket();
-
 		int 		mPort;
 		std::string	mAddress;
-		int 		mSocketDesciptor;
-}
+		int 		mSocket;
+};
+
+class ClientSocket : public ASocket
+{
+	public:
+		virtual ~ClientSocket();
+		ClientSocket();
+		ClientSocket(const int port);
+		ClientSocket(const int port, const char * address);
+		ClientSocket(const ClientSocket & other);
+
+		ClientSocket & operator = (const ClientSocket * other);
+
+		virtual int	handle();
+
+};
 
 class ServerSocket : public ASocket
 {
 	public:
 		virtual ~ServerSocket();
+		ServerSocket();
 		ServerSocket(const int port);
 		ServerSocket(const int port, const char * address);
 		ServerSocket(const ServerSocket & other);
-		ServerSocket& operator = (const ServerSocket * other);
+
+		ServerSocket & operator = (const ServerSocket * other);
 
 		virtual int	handle();
 
-	private:
-		ServerSocket();
-}
+};
+
+

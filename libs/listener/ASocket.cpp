7feee2dc6f0ab93cfd3ca3cb4d32770bf23e012a
Seongmin Jo <seojo@c5r3s1.42seoul.kr>
2023-10-19 17:05:28 +0900

    update: ASocket copy constructor 	"public" -> "private"

--- libs/listener/ASocket.cpp
+++ libs/listener/ASocket.cpp
@@ -26,18 +26,3 @@ ASocket::ASocket(const int port, const char * address)
 	mAddress(address)
 {
 }
-
-ASocket::ASocket(const ASocket & other)
-{
-	*this = other;
-}
-
-ASocket & ASocket::operator = (const ASocket & other)
-{
-	if (this != &other)
-	{
-		this->mPort = other.mPort;
-		this->mAddress = other.mAddress;
-	}
-	return (*this);
-}

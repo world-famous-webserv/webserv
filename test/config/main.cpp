bab8f55e981f3ff5cec687b3004ca1465deffcd0
juwkim <gimjuwon-ui-MacBookPro.local>
2023-11-01 05:48:16 +0900

    Feat: add config test

---
+++ test/config/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "config.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return EXIT_FAILURE;
+    
+    Config config(argv[1]);
+
+    std::cout << config.get("server listen") << '\n';
+    std::cout << config.get("server server_name") << '\n';
+    std::cout << config << '\n';
+    return EXIT_SUCCESS;
+}

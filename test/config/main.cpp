a91d48308fefe8a47351c565b0f1443f2373cf49
juwkim <gimjuwon-ui-MacBookPro.local>
2023-11-03 01:48:43 +0900

    Feat: test code edit

--- test/config/main.cpp
+++ test/config/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "../../libs/config/config.hpp"
 
 int main(int argc, char *argv[])
@@ -8,8 +9,17 @@ int main(int argc, char *argv[])
     
     Config config(argv[1]);
 
-    std::cout << config.get("server listen") << '\n';
-    std::cout << config.get("server server_name") << '\n';
-    std::cout << config << '\n';
+    // localhost라는 이름을 가진 server의 listen 값을 vector에 담아 반환
+    // localhost라는 이름을 가진 server가 여러개일 경우를 고려하여 설계
+    std::string serverName = "localhost";
+    std::string key = "listen";
+
+    std::vector<std::string> v = config.get(serverName, key);
+    std::vector<std::string>::const_iterator it = v.begin();
+
+    while (it != v.end()) {
+        std::cout << serverName << " = " << *it << '\n';
+        ++it;
+    }
     return EXIT_SUCCESS;
 }

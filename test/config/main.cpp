f8fb164b84c5d4bffd1674b5caf04a3b5b452d3d
juwkim <gimjuwon-ui-MacBookPro.local>
2023-11-12 10:07:32 +0900

    test: config test code edit

--- test/config/main.cpp
+++ test/config/main.cpp
@@ -5,21 +5,24 @@
 int main(int argc, char *argv[])
 {
     if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " [configuration file]" << std::endl;
         return EXIT_FAILURE;
-    
+    }
+
     Config config(argv[1]);
 
     // localhost라는 이름을 가진 server의 listen 값을 vector에 담아 반환
     // localhost라는 이름을 가진 server가 여러개일 경우를 고려하여 설계
-    const Config::serverNameT serverName = "localhost";
-    const Config::keyT key = "listen";
+    // const Config::serverNameT serverName = "localhost";
+    // const Config::keyT key = "listen";
 
-    const std::vector<Config::valueT> v = config.get(serverName, key);
-    std::vector<Config::valueT>::const_iterator it = v.begin();
+    // const std::vector<Config::valueT> v = config.get(serverName, key);
+    // std::vector<Config::valueT>::const_iterator it = v.begin();
 
-    while (it != v.end()) {
-        std::cout << serverName << " = " << *it << '\n';
-        ++it;
-    }
+    // while (it != v.end()) {
+    //     std::cout << serverName << " = " << *it << '\n';
+    //     ++it;
+    // }
     return EXIT_SUCCESS;
 }

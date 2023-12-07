1c700ad0d449a9725593c680b0b3a8bf64feec9f
seojo <71538487+joseoungmin@users.noreply.github.com>
2023-12-07 22:23:09 +0900

    쿠키와 세션 (#49)
    * feat: cookie & sesstion
    
    * fix: session.cpp->makeSessionId()

---
+++ src/http/cookie.hpp
@@ -0,0 +1,8 @@
+#ifndef SRC_HTTP_COOKIE_HPP_
+#define SRC_HTTP_COOKIE_HPP_
+#include <string>
+#include <ctime>
+
+std::string makeCookie(const std::string & key, const std::string & val);
+
+#endif /* SRC_HTTP_COOKIE_HPP_ */

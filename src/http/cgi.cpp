df103490c1ab8efbcc34afc68b66a26b605b9efa
iijung <987kmj4@naver.com>
2023-12-13 03:48:39 +0900

    fix: cgi 100000000 error

--- src/http/cgi.cpp
+++ src/http/cgi.cpp
@@ -6,7 +6,14 @@
 
 Cgi::~Cgi(void)
 {
-	this->Update();
+	std::cerr << "Cgi::Close - " << request_.method() << std::endl;
+	int status = 0;
+	int ret = waitpid(pid_, &status, WNOHANG);
+	if (ret == 0) {
+		kill(pid_, SIGKILL);
+		ret = waitpid(pid_, &status, WNOHANG);
+	}
+	response_.set_done(true);
 }
 
 Cgi::Cgi(const location_t& location, HttpRequest& req, HttpResponse& res):
@@ -14,7 +21,7 @@ Cgi::Cgi(const location_t& location, HttpRequest& req, HttpResponse& res):
 	request_(req),
 	response_(res)
 {
-	std::cout << "Cgi::Open" << std::endl;
+	std::cerr << "Cgi::Open - " << request_.method() << std::endl;
 	this->Open();
 }
 
@@ -39,12 +46,14 @@ void Cgi::Child(void)
 	const std::string url = request_.uri();
     const std::string file = location_.root + url.substr(location_.name.length(), url.length() - location_.name.length());
 	const std::string program = GetCgi(location_, file);
-	std::cout << "program = " << program << std::endl;
-	std::cout << "file = " << file << std::endl;
+	std::cerr << "program = " << program << std::endl;
+	std::cerr << "file = " << file << std::endl;
+
+	if (chdir(location_.root.c_str()) == -1)
+		std::exit(1);
 	// args
 	std::vector<std::string> args;
 	args.push_back(program);
-	args.push_back(file);
 
 	std::vector<char*> args_p;
 	for (std::size_t i = 0; i < args.size(); ++i)
@@ -53,6 +62,7 @@ void Cgi::Child(void)
 
 	// environment
 	std::vector<std::string> envp;
+	envp.push_back("PATH_INFO=" + file);
 	if (request_.header("Auth-Scheme") != "")
 		envp.push_back("AUTH_TYPE=" + request_.header("Authorization"));
 	std::stringstream len;
@@ -60,7 +70,8 @@ void Cgi::Child(void)
 	envp.push_back("CONTENT_LENGTH=" + len.str());
 	envp.push_back("CONTENT_TYPE=" + request_.header("Content-Type"));
 	envp.push_back("GATEWAY_INTERFACE=CGI/1.1");
-	envp.push_back("QUERY_STRING=" + request_.header("Query"));
+	if (request_.header("Query").empty() == false)
+		envp.push_back("QUERY_STRING=" + request_.header("Query"));
 	envp.push_back("REMOTE_HOST=" + request_.header("Authorization"));
 	envp.push_back("REMOTE_USER=" + request_.header("Authorization"));
 	envp.push_back("REQUEST_METHOD=" + request_.method());
@@ -72,7 +83,13 @@ void Cgi::Child(void)
 		envp_p.push_back(const_cast<char*>(envp[i].c_str()));
 	envp_p.push_back(NULL);
 
+std::cerr << "argx[x] " << args[0] << std::endl;
+	for (std::size_t i = 0; i < envp.size(); ++i)
+std::cerr << "envp[" << i << "] " << envp[i] << std::endl;
+
+std::cerr << "execute" << std::endl;
 	execve(args[0].c_str(), &args_p[0], &envp_p[0]);
+std::cerr << "execute fail" << std::endl;
 	std::exit(EXIT_FAILURE);
 }
 
@@ -80,16 +97,17 @@ void Cgi::Child(void)
 // Main
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <sys/socket.h>
 void Cgi::Open()
 {
-	std::cout << "Cgi::Open" << std::endl;
-
 	int fd[2];
 	if (socketpair(AF_UNIX, SOCK_STREAM, 0, fd) == -1) {
 		std::cerr << "- socketpair error: " << strerror(errno) << std::endl;
 		response_.set_status(kInternalServerError);
 		return;
 	}
+	fcntl(fd[0], F_SETFL, O_NONBLOCK, FD_CLOEXEC);
 	pid_ = fork();
 	if (pid_ == -1) {
 		std::cerr << "- fork erorr: " << strerror(errno) << std::endl;
@@ -117,49 +135,49 @@ void Cgi::Read(void)
 {
 	if (identifier_ == -1)
 		return ;
-	char buf[1024] = {0};
-	ssize_t nbytes = recv(identifier_, buf, sizeof(buf), 0);
+	char buf[1024 * 1024] = {0};
+	ssize_t nbytes = recv(identifier_, buf, sizeof(buf), MSG_DONTWAIT);
 	if (nbytes < 0) {
 		response_.set_status(kInternalServerError);
 		return this->Broken(errno);
 	}
-	if (nbytes == 0) return this->Update();
+	if (nbytes == 0) return this->Close();
 	// put
+	response_.body().clear();
 	response_.body().write(buf, nbytes);
+//	std::cerr << "read total: " << response_.body().tellp() << std::endl;
 }
 
 void Cgi::Write(void)
 {
 	if (identifier_ == -1)
 		return;
+	if (request_.body().tellp() == 0) {
+		shutdown(identifier_, SHUT_WR);	// not allow function, but need half-close
+		return;
+	}
+	std::streampos curr = request_.body().tellg();
 	// get
-	char buf[1024] = {0};
-	request_.body().clear();
+	char buf[1024 * 1024] = {0};
 	request_.body().read(buf, sizeof(buf));
-	if (request_.body().gcount() <= 0) return;
+	if (request_.body().gcount() < 0) return;
 	// send
-	ssize_t nbytes = send(identifier_, buf, request_.body().gcount(), 0);
+	ssize_t nbytes = send(identifier_, buf, request_.body().gcount(), MSG_DONTWAIT);
+	if (nbytes > 0) {
+		request_.body().clear();
+		request_.body().seekg(curr);
+		request_.body().seekg(nbytes, std::ios::cur);
+//	std::cerr << "write total: " << request_.body().tellg() << std::endl;
+		return;
+	}
+	request_.body().clear();
+	request_.body().seekg(curr);
 	if (nbytes < 0) {
 		response_.set_status(kInternalServerError);
 		return this->Broken(errno);
 	}
-	if (nbytes == 0) return this->Update();
-	request_.body().seekg(nbytes, std::ios::cur);
 }
 
 void Cgi::Update(void)
 {
-	if (pid_ == -1) return;
-	int status = 0;
-	const pid_t ret = waitpid(pid_, &status, WNOHANG);
-	if (ret < 0) {
-		return response_.set_status(kInternalServerError);
-	} else if (ret != 0) {
-		pid_ = -1;
-		if (WIFEXITED(status))
-			response_.set_done(true);
-		else
-			response_.set_status(kInternalServerError);
-		this->Close();
-	}
 }

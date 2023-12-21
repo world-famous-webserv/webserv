2063a72aa957289b722e6328248d1921931ffb1e
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-21 16:44:32 +0900

    Conf 구조 수정 (#65)
    * Fix: conf structure edit
    
    * Feat: error page
    
    * remove: debug message
    
    ---------
    
    Co-authored-by: juwkim <gimjuwon-ui-MacBookPro.local>
    Co-authored-by: minjungk <55140902+iijung@users.noreply.github.com>

--- libs/config/conf.hpp
+++ libs/config/conf.hpp
@@ -18,33 +18,10 @@ class Conf
         int GetLocationIdx(const std::string &url) const;
         location_t GetLocation(const int idx) const;
 
-        bool sendfile;
-        bool autoindex;
-        bool absolute_redirect;
-        bool server_name_in_redirect;
-        bool tcp_nodelay;
-        bool tcp_nopush;
-        std::string default_type;
-        std::string root;
-        std::vector<std::string> allows;
-        std::vector<std::string> denys;
         struct linger linger;
-        int send_lowat;
-        int sendfile_max_chunk;
-        int client_max_body_size;
-        int client_body_timeout;
-        int client_header_timeout;
-        int client_header_buffer_size;
-        int keepalive_requests;
-        int keepalive_time;
-        keepalive_timeout_t keepalive_timeout;
-        std::vector<std::string> index;
         std::vector<std::string> server_name;
         std::vector<struct location_s> locations;
         std::map<int, std::string> error_page;
-        std::map<std::string, std::string> fastcgi_param;
-        try_files_t try_files;
-        return_t ret;
         listen_t listen;
     private:
         Conf();
